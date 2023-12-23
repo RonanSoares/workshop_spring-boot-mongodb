5f5ce34cd597bfca605e91f719eea4159926d112
Ronan Soares <ronanp2@hotmail.com>
2023-12-23 11:01:40 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/ronansoares/workshopmongo/resources/UserResource.java
+++ src/main/java/com/ronansoares/workshopmongo/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.ronansoares.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ronansoares.workshopmongo.domain.User;
+import com.ronansoares.workshopmongo.dto.UserDTO;
 import com.ronansoares.workshopmongo.services.UserService;
 
 @RestController                   // Para falar que essa classe seja um Recurso Rest
@@ -20,8 +23,10 @@ public class UserResource {
 	
 	// Método para retornar uma lista de usuários (busca todos)
 	@RequestMapping(method = RequestMethod.GET)       // Para avisar que esse método é um end point para esse caminho
-	public ResponseEntity<List<User>> findAll(){      // Resp.Ent. para retornar resposta http completa com cabeçalhos erros etc
+	public ResponseEntity<List<UserDTO>> findAll(){      // Resp.Ent. para retornar resposta http completa com cabeçalhos erros etc
 		List<User> list = service.findAll();           // lista busca os usuário do BD e adiciona na lista
-		return ResponseEntity.ok().body(list);        // Retorna resposta http com sucesso
+		// Converte a list do tipo User para UserDTO. Depois volta o stream para uma list.
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);        // Retorna resposta http com sucesso
 	}
 }
